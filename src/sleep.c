381d1947ac5b284752ce5c2c4e3c5cfb82ebaf1b
Nakidai <nakidai@disroot.org>
2025-04-01 00:00:35 +0300

    Math D:

--- src/sleep.c
+++ src/sleep.c
@@ -7,7 +7,7 @@ void sleepMS(int msec)
     struct timespec tosleep =
     {
         .tv_sec = msec / 1000,
-        .tv_nsec = (msec * 1000000) % 1000,
+        .tv_nsec = msec % 1000 * 1000000,
     };
     thrd_sleep(&tosleep, NULL);
 }
